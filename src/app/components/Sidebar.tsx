3b3a8b9ded02ca6aa61324231c2e6e5e633cd00a
Newton Bofulo <newtonombese1@gmail.com>
2024-04-12 23:37:12 +0300

    Improving the sidebar component

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,17 +1,19 @@
+import { openAddAndEditBoardModal } from "@/components/redux/features/appSlice";
+
 export default function Sidebar() {
-  return (
-    <aside className="w-[18.75rem] flex-none dark:bg-dark-grey h-full py-6 pr-6">
-      <p className="text-medium-grey pl-[2.12rem] text-[.95rem] font-semibold uppercase pb-3">
-        {`All Boards (0)`}
+ // rest of code here
+return (
+  <aside className="w-[18.75rem] flex-none dark:bg-dark-grey h-full py-6 pr-6">
+    {/* rest of code here */}
+    {/* trigger the create new board modal */}
+    <button
+      onClick={() => dispatch(openAddAndEditBoardModal("Add New Board"))}
+      className="flex items-center space-x-2 pl-[2.12rem] py-3"
+    >
+      <p className="text-base font-bold capitalize text-main-purple">
+        + Create New Board
       </p>
-      <div className="cursor-pointer flex items-center rounded-tr-full rounded-br-full bg-blue-500 space-x-2 pl-[2.12rem] py-3 pb-3">
-        <p className="text-white text-lg capitalize">Current board name</p>
-      </div>
-      <button className="flex items-center space-x-2 pl-[2.12rem] py-3">
-        <p className="text-base font-bold capitalize text-main-purple">
-          + Create New Board
-        </p>
-      </button>
-    </aside>
-  );
+    </button>
+  </aside>
+);
 }
