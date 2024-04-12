de256c680251201a073ae6bd8af47c13e76ad226
Newton Bofulo <newtonombese1@gmail.com>
2024-04-12 23:35:26 +0300

    Improving the appSlice redux file

--- src/redux/features/appSlice.ts
+++ src/redux/features/appSlice.ts
@@ -1,57 +1,42 @@
-import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
-import { getSession } from "next-auth/react";
-// additionally import the doc and updateDoc method from firestore to get user document reference and update the document, respectively
-import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
-import { db } from "@/components/app/utils/firebaseConfig";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 
-export const fireStoreApi = createApi({
-  reducerPath: "firestoreApi",
-  baseQuery: fakeBaseQuery(),
-  tagTypes: ["Tasks"],
-  endpoints: (builder) => ({
-    fetchDataFromDb: builder.query<{ [key: string]: any }[], void>({
-      async queryFn() {
-        try {
-          const session = await getSession();
-          if (session?.user) {
-            const { user } = session;
-            const ref = collection(db, `users/${user.email}/tasks`);
-            const querySnapshot = await getDocs(ref);
-            return { data: querySnapshot.docs.map((doc) => doc.data()) };
-          }
-        } catch (e) {
-          return { error: e };
-        }
-      },
-      providesTags: ["Tasks"],
-    }),
-    // endpoint for CRUD actions
-    updateBoardToDb: builder.mutation({
-      async queryFn(boardData) {
-        try {
-          const session = await getSession();
-          if (session?.user) {
-            const { user } = session;
-            const ref = collection(db, `users/${user.email}/tasks`);
-            const querySnapshot = await getDocs(ref);
-            const boardId = querySnapshot.docs.map((doc) => {
-              return doc.id;
-            });
-            await updateDoc(doc(db, `users/${user.email}/tasks/${boardId}`), {
-              boards: boardData,
-            });
-          }
-          return { data: null };
-        } catch (e) {
-          return { error: e };
-        }
-      },
-      invalidatesTags: ["Tasks"], // this will be used to invalidate the initially fetched data. 
-      // Data will have to be refetched once this enpoint has been called
-    }),
-  }),
+const initialState = {
+currentBoardName: "",
+// Manage the state for opening and closing the Add and Edit Board modal
+isAddAndEditBoardModal: { isOpen: false, variant: "" },
+};
+
+export const features = createSlice({
+ name: "features",
+ initialState,
+
+ reducers: {
+  setCurrentBoardName: (state, action: PayloadAction<string>) => {
+   state.currentBoardName = action.payload;
+ },
+ // Open the Add and Edit Board modal with a specified variant (add or edit)
+ openAddAndEditBoardModal: (state, { payload }) => {
+   state.isAddAndEditBoardModal.isOpen = true;
+   // Set the kind of modal to open (add board or edit board) based on the variant parameter
+   state.isAddAndEditBoardModal.variant = payload;
+ },
+ // Close the Add and Edit Board modal
+ closeAddAndEditBoardModal: (state) => {
+   state.isAddAndEditBoardModal.isOpen = false;
+   state.isAddAndEditBoardModal.variant = "";
+ },
+},
 });
-
-// Export hooks for using the created endpoint
-export const { useFetchDataFromDbQuery, useUpdateBoardToDbMutation } =
-  fireStoreApi;
+export const {
+setCurrentBoardName,
+openAddAndEditBoardModal,
+closeAddAndEditBoardModal,
+} = features.actions;
+export const getCurrentBoardName = (state: RootState) => state.features.currentBoardName;
+// Selector functions to retrieve isOpen value of state from the isAddAndRditBoardModal state
+export const getAddAndEditBoardModalValue = (state: RootState) => state.features.isAddAndEditBoardModal.isOpen;
+// Selector functions to retrieve isOpen value of state from the isAddAndRditBoardModal state
+export const getAddAndEditBoardModalVariantValue = (state: RootState) => state.features.isAddAndEditBoardModal.variant;
+// Export the reducer for use in the Redux store
+export default features.reducer;
