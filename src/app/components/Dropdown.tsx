eac5c72cf3f6a37db79b6751ff9ebd956e17babf
Newton Bofulo <newtonombese1@gmail.com>
2024-04-12 23:34:55 +0300

    Update the dropdown component file

--- src/app/components/Dropdown.tsx
+++ src/app/components/Dropdown.tsx
@@ -1,19 +1,32 @@
+import { useAppDispatch } from '@/components/redux/hooks'
+import { openAddAndEditBoardModal } from '@/components/redux/features/appSlice';
+
 interface IDropdown {
-  show: boolean;
+ show: boolean
 }
 
 export default function Dropdown({ show }: IDropdown) {
-  return (
-    <div
-      className={`${show ? "block" : "hidden"} w-48 absolute top-full bg-white
-       border shadow-lg right-0 py-2 rounded-2xl`}
-    >
-      <div className="hover:bg-gray-300">
-        <button className="text-sm px-4 py-2">Edit Board</button>
-      </div>
-      <div className="hover:bg-gray-300">
-        <button className="text-sm px-4 py-2">Delete Board</button>
-      </div>
-    </div>
-  );
-}
+
+ const dispatch = useAppDispatch()
+ 
+ return (
+   <div
+     className={`${
+       show ? "block" : "hidden"
+     } w-48 absolute top-full bg-white
+      border shadow-lg right-0 py-2 rounded-2xl`}
+   >
+     <div className="hover:bg-gray-300">
+ 	{/* trigger Edit Board modal here */}
+       <button
+        onClick={() => dispatch(openAddAndEditBoardModal('Edit Board'))}
+        className="text-sm px-4 py-2">Edit Board</button>
+     </div>
+     <div className="hover:bg-gray-300">
+       <button className="text-sm px-4 py-2">
+         Delete Board
+       </button>
+     </div>
+   </div>
+ );
+} 
